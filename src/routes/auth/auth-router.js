adfa7d1684fb4c5db44d1704cf270b38334531c8
bleek42 <brandonleek42@gmail.com>
2020-08-24 00:40:24 -0400

    auth object & router completed

---
+++ src/routes/auth/auth-router.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const AuthService = require('./auth-router');
+
+const authRouter = express.Router();
+// const authService = new AuthService();
+
+authRouter.route('/login').post(async (req, res, next) => {
+  const { id, username, password, email } = req.body;
+
+  const reqUser = { id, username, password, email };
+
+  for (const [key, value] of Object.entries(reqUser)) {
+    if (!value) {
+      res.status(400).json({
+        message: `${key} missing ${value} in request body!`,
+      });
+    }
+    try {
+      const db = req.app.get('db');
+      const authService = new AuthService();
+      const userInDb = await authService.getUserName(db, reqUser.username);
+      if (!userInDb) {
+        res.status(400).json({
+          message: 'Incorrect username and/or password!',
+        });
+      }
+      const isPassword = await authService.comparePassword(
+        reqUser.password,
+        userInDb.password
+      );
+      if (!isPassword) {
+        res.status(400).json({
+          message: 'Incorrect username and/or passsword!',
+        });
+      }
+      const subject = userInDb.id;
+      const payload = {
+        id: userInDb.id,
+      };
+      res.status(200).json({
+        authToken: authService.createJwt(subject, payload),
+        id: userInDb.id,
+      });
+    } catch (error) {
+      res.status(error.statusCode).send(error.message);
+      next();
+    }
+  }
+});
+
+module.exports = authRouter;
